08607a18e34af5139c70cbe67d18212454f0a20b
Ritika Mishra <mishra.r.0804@gmail.com>
2023-11-24 13:58:31 +0530

    Shortest Path Problem

---
+++ path.java
@@ -0,0 +1,33 @@
+public class path {
+    public static double shortestDistance(String str){
+        int x=0;
+        int y=0;
+        for(int i=0;i<str.length()-1;i++){
+
+         if(str.charAt(i)=='N'){
+            y++;
+         }
+         if(str.charAt(i)=='S'){
+            y--;
+         }
+         if(str.charAt(i)=='E'){
+            x++;
+         }
+         if(str.charAt(i)=='W'){
+            x--;
+         }
+         
+        }
+
+        int xDist = x*x;
+        int yDist = y*y;
+        double dist = Math.sqrt(xDist+yDist);
+
+     return  dist;
+    }
+
+    public static void main(String[] args) {
+        String str="NNNWEEW";
+        System.out.println(shortestDistance(str));
+    }
+}
